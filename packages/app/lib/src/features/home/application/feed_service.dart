4710c6c26cf4a79b8880a468134718a6059557bc
Eli <88557639+lishaduck@users.noreply.github.com>
2024-09-23 22:57:58 -0500

    style: more accurately name the `addPosts` method

--- packages/app/lib/src/features/home/application/feed_service.dart
+++ packages/app/lib/src/features/home/application/feed_service.dart
@@ -38,7 +38,7 @@ base class FeedService extends _$FeedService {
   }
 
   /// Replace the current posts with newly generated posts.
-  Future<void> replacePosts(List<PostEntity> newPosts) async {
+  Future<void> addPosts(List<PostEntity> newPosts) async {
     // You can only change a Notifier's `state` by adding methods that assign a new value.
     // You can't mutate the state directly, nor can you change it outside of a method.
     if (state case AsyncData<FeedModel>(:final value)) {
