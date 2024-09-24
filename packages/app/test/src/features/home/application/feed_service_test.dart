c5d30251c9ba378a2d920ae5fd7e5120983e61d9
Eli <88557639+lishaduck@users.noreply.github.com>
2024-09-23 22:58:21 -0500

    chore: add tests for feed_service

---
+++ packages/app/test/src/features/home/application/feed_service_test.dart
@@ -0,0 +1,47 @@
+import 'package:checks/checks.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nexus/src/features/home/application/feed_service.dart';
+import 'package:nexus/src/features/home/domain/feed_entity.dart';
+import 'package:nexus/src/features/home/domain/post_entity.dart';
+
+import '../../../../helpers/riverpod.dart';
+
+void main() {
+  group('Feed service', () async {
+    test('allows mutating the posts locally', () async {
+      // Arrange
+      final container = createContainer();
+      final feedServiceSubscription = container.listen(
+        feedServiceProvider(const FeedEntity.world(), 1),
+        (_, __) {},
+      );
+
+      // Assert
+      check(feedServiceSubscription.read())
+          .isData()
+          .has((i) => i.posts, 'posts')
+          .length
+          .equals(20);
+
+      // Act
+      final postsToAdd = List.generate(
+        20,
+        (index) => const PostEntity(
+          image: null,
+          body: 'body',
+        ),
+      );
+
+      await container
+          .read(feedServiceProvider(const FeedEntity.world(), 1).notifier)
+          .addPosts(postsToAdd);
+
+      // Assert
+      check(feedServiceSubscription.read())
+          .isData()
+          .has((i) => i.posts, 'posts')
+          .length
+          .equals(40);
+    });
+  });
+}
