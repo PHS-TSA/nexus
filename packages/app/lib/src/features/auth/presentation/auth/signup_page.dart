7467c39bb52ba7a83908fc9b4e42220b0fb05c97
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-09-25 12:52:32 -0500

    Sign up page revamp

--- packages/app/lib/src/features/auth/presentation/auth/signup_page.dart
+++ packages/app/lib/src/features/auth/presentation/auth/signup_page.dart
@@ -15,70 +15,222 @@ class SignupPage extends ConsumerWidget {
     final emailcontroller = TextEditingController();
     final passwordcontroller = TextEditingController();
 
+    final currentWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign up'),
-        automaticallyImplyLeading: false,
-      ),
-      body: Column(
-        children: [
-          TextFormField(
-            controller: namecontroller,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              labelText: 'Name',
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/pictures/login_image.png'),
+            fit: BoxFit.fill, // Need to find a better image or move photo down
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: currentWidth / 4,
+            right: currentWidth / 4,
+            top: 35,
+            bottom: 40,
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(
+                255,
+                34,
+                29,
+                43,
+              ), //TODO better color for this
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  const DecoratedBox(
+                    //TODO Redesign this was for testing
+                    decoration: BoxDecoration(
+                        // color: Colors.white,
+                        // border: Border.all(color: Colors.white),
+                        // borderRadius: BorderRadius.circular(5),
+                        ),
+                    child: Text(
+                      'Welcome to Nexus!',
+                      style: TextStyle(
+                        fontSize: 28,
+                        color: Color.fromARGB(255, 221, 168, 230),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  TextFormField(
+                    controller: namecontroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Name',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: emailcontroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: passwordcontroller,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            // May need to do cool async things here
+                            //login the user
+                            // await ref
+                            //     .read(authRepositoryProvider)
+                            //     .loginUser(
+                            //       emailcontroller.text,
+                            //       passwordcontroller.text,
+                            //     )
+                            //     .then((value) {
+                            //   if (value) {
+                            //     context.router.push(
+                            //       const WrapperRoute(),
+                            //     );
+                            //   } else {
+                            //     ScaffoldMessenger.of(context).showSnackBar(
+                            //       const SnackBar(
+                            //         content: Text('Invalid email password'),
+                            //       ),
+                            //     );
+
+                            //   }
+                            // });
+                            await ref
+                                .read(authRepositoryProvider)
+                                .createUser(
+                                  namecontroller.text,
+                                  emailcontroller.text,
+                                  passwordcontroller.text,
+                                )
+                                .then((value) {
+                              if (value == 'success') {
+                                //TODO Fix routing
+                                context.router.push(const LoginRoute());
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text(value)),
+                                );
+                              }
+                            });
+                            // navigate to the homepage
+                          },
+                          child: const Text('Sign Up'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: TextButton(
+                      onPressed: () {
+                        context.router.push(const LoginRoute());
+                      },
+                      child: const Text('Back to login'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          TextFormField(
-            controller: emailcontroller,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              labelText: 'Email',
-            ),
-          ),
-          TextFormField(
-            controller: passwordcontroller,
-            obscureText: true,
-            decoration: const InputDecoration(
-              labelText: 'Password',
-            ),
-          ),
-          Row(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  ref
-                      .read(authRepositoryProvider)
-                      .createUser(
-                        namecontroller.text,
-                        emailcontroller.text,
-                        passwordcontroller.text,
-                      )
-                      .then((value) {
-                    if (value == 'success') {
-                      //TODO Fix routing
-                      context.router.push(const LoginRoute());
-                    } else {
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(SnackBar(content: Text(value)));
-                    }
-                  });
-                },
-                child: const Text('Sign Up'),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              OutlinedButton(
-                onPressed: () {
-                  context.router.push(const LoginRoute());
-                },
-                child: const Text('Login'),
-              ),
-            ],
-          ),
-        ],
+        ),
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Sign up'),
+    //     automaticallyImplyLeading: false,
+    //   ),
+    //   body: Column(
+    //     children: [
+    //       TextFormField(
+    //         controller: namecontroller,
+    //         keyboardType: TextInputType.emailAddress,
+    //         decoration: const InputDecoration(
+    //           labelText: 'Name',
+    //         ),
+    //       ),
+    //       TextFormField(
+    //         controller: emailcontroller,
+    //         keyboardType: TextInputType.emailAddress,
+    //         decoration: const InputDecoration(
+    //           labelText: 'Email',
+    //         ),
+    //       ),
+    //       TextFormField(
+    //         controller: passwordcontroller,
+    //         obscureText: true,
+    //         decoration: const InputDecoration(
+    //           labelText: 'Password',
+    //         ),
+    //       ),
+    //       Row(
+    //         children: [
+    //           ElevatedButton(
+    //             onPressed: () {
+    //               ref
+    //                   .read(authRepositoryProvider)
+    //                   .createUser(
+    //                     namecontroller.text,
+    //                     emailcontroller.text,
+    //                     passwordcontroller.text,
+    //                   )
+    //                   .then((value) {
+    //                 if (value == 'success') {
+    //                   //TODO Fix routing
+    //                   context.router.push(const LoginRoute());
+    //                 } else {
+    //                   ScaffoldMessenger.of(context)
+    //                       .showSnackBar(SnackBar(content: Text(value)));
+    //                 }
+    //               });
+    //             },
+    //             child: const Text('Sign Up'),
+    //           ),
+    //           const SizedBox(
+    //             width: 20,
+    //           ),
+    //           OutlinedButton(
+    //             onPressed: () {
+    //               context.router.push(const LoginRoute());
+    //             },
+    //             child: const Text('Login'),
+    //           ),
+    //         ],
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
