aada60493c9acf023a7d0f48e11ab88e97732e0b
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-09-26 13:21:37 -0500

    remove auth guard class

--- packages/app/lib/src/app/auth_guard.dart
+++
@@ -1,8 +0,0 @@
-import 'package:auto_route/auto_route.dart';
-
-class AuthGuard extends AutoRouteGuard{
-
-
-  @override
-  Future<void> onNavigation(NavigationResolver resolver, StackRouter router)
-}
