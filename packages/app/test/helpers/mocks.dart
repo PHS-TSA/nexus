cf2390a0b78980a7de6c461fb94343af6adffcfa
Eli <88557639+lishaduck@users.noreply.github.com>
2024-10-03 13:16:29 -0500

    fix: todo

--- packages/app/test/helpers/mocks.dart
+++ packages/app/test/helpers/mocks.dart
@@ -8,22 +8,46 @@ class MockSharedPreferences extends Mock
 
 class MockAuthRepository extends Mock implements AuthRepository {}
 
-// TODO: Grab fake data from appwrite docs.
+/// A fake user.
+/// Taken from the [Appwrite docs](https://appwrite.io/docs/references/cloud/models/user).
 final fakeUser = User(
-  $createdAt: '0',
-  $id: 'a',
-  $updatedAt: '0',
-  name: '',
-  registration: '0',
+  $id: '5e5ea5c16897e',
+  $createdAt: '2020-10-15T06:38:00.000+00:00',
+  $updatedAt: '2020-10-15T06:38:00.000+00:00',
+  name: 'John Doe',
+  password:
+      // cspell: disable-next-line
+      r'$argon2id$v=19$m=2048,t=4,p=3$aUZjLnliVWRINmFNTWMudg$5S+x+7uA31xFnrHFT47yFwcJeaP0w92L/4LdgrVRXxE',
+  hash: 'argon2',
+  hashOptions: {
+    'type': 'argon2',
+    'memoryCost': 65536,
+    'timeCost': 4,
+    'threads': 3,
+  },
+  registration: '2020-10-15T06:38:00.000+00:00',
   status: true,
-  labels: [],
-  passwordUpdate: '0',
-  email: '0',
-  phone: '0',
-  emailVerification: false,
-  phoneVerification: false,
-  mfa: false,
+  labels: [
+    'vip',
+  ],
+  passwordUpdate: '2020-10-15T06:38:00.000+00:00',
+  email: 'john@appwrite.io',
+  phone: '+4930901820',
+  emailVerification: true,
+  phoneVerification: true,
+  mfa: true,
   prefs: Preferences(data: {}),
-  targets: [],
-  accessedAt: '0',
+  targets: [
+    Target(
+      $id: '259125845563242502',
+      $createdAt: '2020-10-15T06:38:00.000+00:00',
+      $updatedAt: '2020-10-15T06:38:00.000+00:00',
+      name: 'Aegon apple token',
+      userId: '259125845563242502',
+      providerId: '259125845563242502',
+      providerType: 'email',
+      identifier: 'token',
+    ),
+  ],
+  accessedAt: '2020-10-15T06:38:00.000+00:00',
 );
