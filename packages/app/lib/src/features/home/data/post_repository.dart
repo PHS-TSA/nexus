c090f457a414d4df05404c2071fb26e684dcc5f2
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-10-07 13:58:48 -0500

    implemented all post code from post branch except for like button, save button, and tests(already added i beleive)

--- packages/app/lib/src/features/home/data/post_repository.dart
+++ packages/app/lib/src/features/home/data/post_repository.dart
@@ -1,20 +1,31 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../env/env.dart';
+import '../../../utils/api.dart';
 
 part 'post_repository.g.dart';
 
 /*
 How to do things:
 Put database and collection id in .env
-
-
-
+Implement post code from other branch
+For first implementation have post_repository read in all posts and then add queries and other filters down the line
+Have flutter access user location
 */
+
+// Post_entitys are put into list in feed_service
 
 abstract interface class PostRepository {
   //Have funcs in heres
 }
 
-final class _AppwritePostRepository implements PostRepository {}
+final class _AppwritePostRepository implements PostRepository {
+  String databaseId = Env.databaseId;
+  String collectionId = Env.postsCollectionId;
+
+  final Databases database = Databases(client as Client);
+}
 
 @riverpod
 PostRepository postRepository(PostRepositoryRef ref) {
--- packages/app/lib/src/features/home/presentation/home/feed.dart
+++ packages/app/lib/src/features/home/presentation/home/feed.dart
@@ -4,6 +4,7 @@ library;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:latlong2/latlong.dart';
 
 import '../../application/feed_service.dart';
 import '../../domain/feed_entity.dart';
@@ -29,7 +30,16 @@ class Feed extends ConsumerWidget {
         onPressed: () {},
       ),
       body: ListView.builder(
-        prototypeItem: const _Post(post: PostEntity(body: '', image: null)),
+        prototypeItem: _Post(
+          post: PostEntity(
+            headline: '',
+            body: '',
+            image: null,
+            location: const LatLng(0, 0),
+            timestamp: DateTime.fromMicrosecondsSinceEpoch(0, isUtc: true),
+            author: const UserId(''),
+          ),
+        ),
         itemBuilder: (context, index) {
           // Calculate the page and index in the page.
           final page = index ~/ pageSize + 1;
@@ -46,8 +56,12 @@ class Feed extends ConsumerWidget {
             // If there's an error, display it as another post.
             AsyncError(:final error) => _Post(
                 post: PostEntity(
+                  headline: 'Error',
                   body: error.toString(),
                   image: null,
+                  author: const UserId(''),
+                  location: const LatLng(0, 0),
+                  timestamp: DateTime.now(),
                 ),
               ),
             // If we're loading, display a loading indicator.
@@ -86,7 +100,13 @@ class _Post extends StatelessWidget {
           // Else, return null.
           null => null,
         },
-        title: Text(post.body),
+        title: Text(post.headline),
+        subtitle: switch (post.body) {
+          // If the body is not null, use it as the content.
+          final String body => Text(body),
+          // Else, return null.
+          null => null,
+        },
       ),
     );
   }
