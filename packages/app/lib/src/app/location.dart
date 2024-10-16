e6bd63662f1c56d249868ca9db5d6a667198801b
Eli <88557639+lishaduck@users.noreply.github.com>
2024-10-15 20:45:11 -0500

    refactor: kinda make a locationrepo
    Just write out the interface for now.
    We can move the function later.

--- packages/app/lib/src/app/location.dart
+++ packages/app/lib/src/app/location.dart
@@ -1,11 +1,14 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:geolocator/geolocator.dart';
 
 /// Determine the current position of the device.
 ///
 /// When the location services are not enabled or permissions
 /// are denied the `Future` will return an error.
 
-class LocationRepository {
-  @riverpod
-  LocationRepository locationRepository() => LocationRepository();
+abstract interface class LocationRepository {
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> determinePosition();
 }
