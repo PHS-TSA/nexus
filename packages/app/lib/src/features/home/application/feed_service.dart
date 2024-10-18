a2ca95bac62a932f292d03d5712ef715d56edc0b
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-10-18 14:02:03 -0500

    wip

--- packages/app/lib/src/features/home/application/feed_service.dart
+++ packages/app/lib/src/features/home/application/feed_service.dart
@@ -1,4 +1,6 @@
 /// This library provides a service to stream posts in DB to the UI.
+// ignore_for_file: prefer_expression_function_bodies
+
 library;
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
