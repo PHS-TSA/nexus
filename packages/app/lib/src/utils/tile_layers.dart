b6b9a03f8d10632db8bf45160afeab02c7ae2279
Eli <88557639+lishaduck@users.noreply.github.com>
2025-01-03 21:43:05 -0600

    fix: mock dio in tests
    It no longer loudly spits out errors!

--- packages/app/lib/src/utils/tile_layers.dart
+++ packages/app/lib/src/utils/tile_layers.dart
@@ -3,7 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 
 /// Create an TileLayer for OpenStreetMap.
-TileLayer openStreetMapLayer([Dio? dio]) {
+TileLayer openStreetMapLayer(Dio dio) {
   return TileLayer(
     urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     userAgentPackageName: 'org.psdr3.nexus',
