2353ea333e611f4741a5d039fb792abf7d5c94b8
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-19 20:49:46 -0500

    feat: optimized mobile login ui

--- packages/app/lib/src/features/auth/presentation/auth/log_in_page.dart
+++ packages/app/lib/src/features/auth/presentation/auth/log_in_page.dart
@@ -226,99 +226,100 @@ class _MobileLogInPage extends HookConsumerWidget {
     }, [formKey]);
     // TODO: implement build
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(
-          left: MediaQuery.sizeOf(context).width / 4,
-          right: MediaQuery.sizeOf(context).width / 4,
-          top: MediaQuery.sizeOf(context).height / 6,
-          bottom: MediaQuery.sizeOf(context).height / 6,
-        ),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: Assets.pictures.loginImage.provider(),
-            // TODO(MattsAttack): I need to find a better image or move the photo down.
-            fit: BoxFit.fill,
-          ),
-        ),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    Text(
-                      'Welcome to Town Talk!',
-                      style: TextStyle(
-                        fontSize: 28,
-                        color: Theme.of(context).colorScheme.onPrimaryContainer,
-                      ),
-                    ),
-                    const SizedBox(height: 32),
-                    TextFormField(
-                      initialValue: email.value,
-                      onSaved: (value) {
-                        if (value == null) return;
-
-                        email.value = value;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextFormField(
-                      initialValue: password.value,
-                      onSaved: (value) {
-                        if (value == null) return;
-
-                        password.value = value;
-                      },
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      keyboardType: TextInputType.visiblePassword,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Center(
-                      child: SizedBox(
-                        // Makes the button take up the full width of its parent.
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          // TODO(MattsAttack): I don't don't love the button color, it could be improved.
-                          onPressed: handleSubmit,
-                          child: const Text('Log in'),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16),
-                      child: TextButton(
-                        onPressed: () async {
-                          await context.router.push(
-                            SignUpRoute(onResult: _onResult),
-                          );
-                        },
-                        child: const Text("Don't have an account? Sign up!"),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: Assets.pictures.loginImage.provider(),
+              fit: BoxFit.cover,
             ),
-          ],
+          ),
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Welcome to Town Talk!',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color:
+                                Theme.of(
+                                  context,
+                                ).colorScheme.onPrimaryContainer,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 24),
+                        TextFormField(
+                          initialValue: email.value,
+                          onSaved: (value) {
+                            if (value == null) return;
+                            email.value = value;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        TextFormField(
+                          initialValue: password.value,
+                          onSaved: (value) {
+                            if (value == null) return;
+                            password.value = value;
+                          },
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: handleSubmit,
+                            child: const Text('Log in'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: TextButton(
+                            onPressed: () async {
+                              await context.router.push(
+                                SignUpRoute(onResult: _onResult),
+                              );
+                            },
+                            child: const Text(
+                              "Don't have an account? Sign up!",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
