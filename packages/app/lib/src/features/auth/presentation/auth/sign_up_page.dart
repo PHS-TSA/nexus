88dd63214e18ef0511e364d81901d0860c96af89
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-26 13:08:07 -0500

    feat: responsive sign up page

--- packages/app/lib/src/features/auth/presentation/auth/sign_up_page.dart
+++ packages/app/lib/src/features/auth/presentation/auth/sign_up_page.dart
@@ -2,7 +2,6 @@
 library;
 
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,6 +9,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../../app/router.gr.dart';
 import '../../../../gen/assets.gen.dart';
 import '../../../../utils/hooks.dart';
+import '../../../../utils/responsive.dart';
 import '../../../../utils/toast.dart';
 import '../../application/auth_service.dart';
 import '../../domain/auth_callback.dart';
@@ -28,6 +28,23 @@ class SignUpPage extends HookConsumerWidget {
 
   final AuthCallback? _onResult;
 
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return context.sizeClass == MaterialWindowSizeClass.compact
+            ? _MobileSignUpPage(onResult: _onResult)
+            : _DesktopSignUpPage(onResult: _onResult);
+      },
+    );
+  }
+}
+
+class _DesktopSignUpPage extends HookConsumerWidget {
+  const _DesktopSignUpPage({super.key, AuthCallback? onResult})
+    : _onResult = onResult;
+
+  final AuthCallback? _onResult;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final formKey = useGlobalKey<FormState>();
@@ -171,15 +188,148 @@ class SignUpPage extends HookConsumerWidget {
       ),
     );
   }
+}
 
-  // coverage:ignore-start
+class _MobileSignUpPage extends HookConsumerWidget {
+  const _MobileSignUpPage({super.key, AuthCallback? onResult})
+    : _onResult = onResult;
+
+  final AuthCallback? _onResult;
   @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(
-      ObjectFlagProperty<AuthCallback?>.has('onResult', _onResult),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = useGlobalKey<FormState>();
+
+    final email = useState('');
+    final password = useState('');
+
+    final handleSubmit = useCallback(() async {
+      if (formKey.currentState?.validate() ?? false) {
+        formKey.currentState?.save();
+
+        // Log in the user.
+        await ref
+            .read(authServiceProvider.notifier)
+            .logInUser(email.value, password.value);
+
+        // Check that the widget still exists after the async operation.
+        if (!context.mounted) return;
+
+        if (ref.read(authServiceProvider).requireValue != null) {
+          if (_onResult != null) {
+            // Navigate the page the user wanted to go to.
+            // Runs the function passed in by the guard and brings user back to previous page.
+            _onResult(didLogIn: true);
+          } else {
+            // Replace the route so user won't come back to login.
+            await context.router.replace(const LocalFeedRoute());
+          }
+        } else {
+          // TODO(lishaduck): Move this to the guard.
+          context.showSnackBar(
+            content: const Text('Invalid username and password'),
+          );
+        }
+      }
+    }, [formKey]);
+
+    // TODO(MattsAttack): implement build.
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: Assets.pictures.loginImage.provider(),
+              fit: BoxFit.cover,
+            ),
+          ),
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Welcome to Town Talk!',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color:
+                                Theme.of(
+                                  context,
+                                ).colorScheme.onPrimaryContainer,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 24),
+                        TextFormField(
+                          initialValue: email.value,
+                          onSaved: (value) {
+                            if (value == null) return;
+                            email.value = value;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        TextFormField(
+                          initialValue: password.value,
+                          onSaved: (value) {
+                            if (value == null) return;
+                            password.value = value;
+                          },
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: handleSubmit,
+                            child: const Text('Log in'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: TextButton(
+                            onPressed: () async {
+                              await context.router.push(
+                                SignUpRoute(onResult: _onResult),
+                              );
+                            },
+                            child: const Text(
+                              "Don't have an account? Sign up!",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
-
-  // coverage:ignore-end
 }
