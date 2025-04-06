5aae8e24bfe2a94b38902734495f184ad91a47ba
Eli <88557639+lishaduck@users.noreply.github.com>
2025-04-06 10:56:40 -0500

    fix: headers are immutable
    Since when‽

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,10 +4,11 @@ export async function respond(req: Request, dir = "static"): Promise<Response> {
   const res = await serveDir(req, {
     fsRoot: dir,
     enableCors: true,
+    headers: [
+      "Cross-Origin-Opener-Policy:same-origin",
+      "Cross-Origin-Embedder-Policy:require-corp",
+    ],
   });
-
-  res.headers.set("Cross-Origin-Opener-Policy", "same-origin");
-  res.headers.set("Cross-Origin-Embedder-Policy", " require-corp");
 
   return res;
 }
